424b7880811ea4dc5eed6ec113cff2a281dc89b0
Daria <daria.bnn@bk.ru>
2022-04-22 09:10:05 +0300

    init commit

--- js/blocks.js
+++ js/blocks.js
@@ -1,7 +1,26 @@
 window.application.blocks['create-form'] = createForm;
 
 function createForm(container) {
-    container.appendChild(
-        createTempleteCart(blockForm())
-    );
+    container.appendChild(createTempleteCart(blockForm()));
+
+    const form = container.querySelector('.form');
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const button = event.target.querySelectorAll('.form__button');
+        const inputs = event.target.querySelectorAll('.block-options__input');
+
+        let checkValidate = false;
+
+        inputs.forEach(input => {
+            if (input.checked) {
+                checkValidate = true;
+            };
+        });
+
+        if (!checkValidate) {
+            console.log('ничего не выбрано')
+        }
+    })
 }
